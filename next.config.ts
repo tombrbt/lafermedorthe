e397d2c95fdc5bc4a7fbc0018094f47bd9a5667a
Hanzo <61524340+hanzopro@users.noreply.github.com>
2026-01-18 17:26:35 +0100

    Fix CSS for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-import { NextConfig } from 'next';
+import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: 'export', // très important pour générer un site statique
-  basePath: '/lafermedorthe', // remplace NOM_REPO par le nom de ton repo GitHub
-  assetPrefix: '/lafermedorthe/', // pour que les assets (images, CSS, JS) soient corrects sur GitHub Pages
-};
+  output: 'export', // pour site statique si besoin
+}
 
-export default nextConfig;
+export default nextConfig
+
